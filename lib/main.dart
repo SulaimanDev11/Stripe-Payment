be4796b619e4e80e0b919cba0fffeeccdc134551
Muhammad Sulaiman <sulaikhan12@gmail.com>
2022-01-17 11:52:56 +0500

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey =
       'pk_test_51KHP15Dv7hqQCbNCwHYu9Q8ny7h9dceL79s53lt4flKZjpNhq7vCZKBDRFDvmFU5ecAz7OfiZBGvjOF8T84bNwKf00oDVYDBGm';
+  await Stripe.instance.applySettings();
   runApp(
     GetMaterialApp(
       title: "Stripe Payment",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(primarySwatch: Colors.blue),
     ),
   );
 }
